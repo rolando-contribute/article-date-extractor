d65fe3b2eb236fe66504a8247165dc6491dbd728
Rolando Espinoza <rndmax84@gmail.com>
2016-10-04 04:11:47 -0300

    Use BS4.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     description='Automatically extracts and normalizes an online article or blog post publication date',
     long_description=readme,
     install_requires=[
-        "BeautifulSoup >= 3.2.1",
+        "BeautifulSoup4 >= 4.5.1",
         "python-dateutil >= 2.4.2"
     ],
     classifiers=(
